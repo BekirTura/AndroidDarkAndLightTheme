e794eb9f7615faea7ec8afa56d38b71b1d1d2776
bekir <l1112031@std.yildiz.edu.tr>
2020-01-31 16:06:56 +0300

    Android app
    Development Dark Mode And LightMode

---
+++ AndroidLightDarkThemeApp/app/src/main/java/com/bekirtura/androidlightdarkthemeapp/MainActivity.java
@@ -0,0 +1,35 @@
+package com.bekirtura.androidlightdarkthemeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnChangeTheme;
+
+    boolean themeLight = true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnChangeTheme = findViewById(R.id.button_change_theme);
+
+        btnChangeTheme.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(themeLight){
+                    getDelegate().setLocalNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    themeLight = false;
+                }else{
+                    getDelegate().setLocalNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    themeLight = false;
+                }
+            }
+        });
+    }
+}
